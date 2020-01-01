652b1b334c55e55472198aa115ae83ef23c7210a
Long Le <hoanglong25588@gmail.com>
2020-01-01 17:55:13 +0100

    convert pagination result to mongoose obj to have virtual props

--- src/api/models/Collection.ts
+++ src/api/models/Collection.ts
@@ -1,8 +1,9 @@
 import * as _ from 'lodash';
 import mongoose from 'mongoose';
-import { prop, getModelForClass, modelOptions, arrayProp, pre } from '@typegoose/typegoose';
-import slugify from 'slugify';
+import { prop, getModelForClass, modelOptions, arrayProp } from '@typegoose/typegoose';
+// import slugify from 'slugify';
 import { BaseSchema, defaultOptions } from './BaseModel';
+import { env } from '../../env';
 
 const schemaOptions = Object.assign(
   {},
@@ -12,30 +13,36 @@ const schemaOptions = Object.assign(
   defaultOptions,
 );
 
-@pre<ICollection>('save', async function(next: (err?: Error) => void): Promise<void> {
-  try {
-    this.slug = slugify(this.name);
-    return next();
-  } catch (error) {
-    return next(error);
-  }
-})
 @modelOptions({ existingMongoose: mongoose, schemaOptions })
 export class ICollection extends BaseSchema {
   @prop({ required: true })
   public name: string;
+
   @prop({ required: true })
   public nativeName: string;
+
   @prop({ required: true })
   public description: string;
+
   @prop({ required: true })
   public thumbnail: string;
+
   @prop({ required: true })
-  public slug: string;
+  public uri: string;
+
   @prop({ required: true })
   public photos: string[];
+
   @arrayProp({ required: true, items: String })
   public tags: string[];
+
+  get thumnailUrl(): string {
+    return `${env.app.uri}/${this.uri}/${this.thumbnail}`;
+  }
+
+  get photosUrl(): string[] {
+    return this.photos.map(photo => `${env.app.uri}/${this.uri}/${photo}`);
+  }
 }
 
 export const Collection = getModelForClass(ICollection);
