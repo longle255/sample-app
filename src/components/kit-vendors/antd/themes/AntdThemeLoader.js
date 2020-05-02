bf782d95552df03ba373110c76160893cbef447d
nick <nick@mediatec.org>
2020-05-02 18:11:14 +0300

    AntdThemeLoader Fix

--- src/components/kit-vendors/antd/themes/AntdThemeLoader.js
+++ src/components/kit-vendors/antd/themes/AntdThemeLoader.js
@@ -1,82 +1,49 @@
-/* eslint-disable no-param-reassign */
-/* eslint-disable class-methods-use-this */
-
-// const preProcessor = ClassConstructor => class PreProcessor extends ClassConstructor {
-//   constructor(options) {
-//     super(options)
-//     this.options = options
-//     this.options.replacement = options.replacement || []
-//     this.less = options.less
-//   }
-
-//   process(css) {
-//     return css
-//   }
-// }
-
-// original code: https://github.com/atvilelas/antd-theming-engine/blob/master/PostImportPrefixCleanUp.js
-const postProcessor = ClassConstructor => class PostProcessor extends ClassConstructor {
-  constructor(options) {
-    super(options)
-    this.options = options
-    this.options.wrappers = options.wrappers || []
-    this.less = options.less
+/* eslint-disable */
+var GetPostProcessor = function () {
+  function Processor(options) {
+    this.options = options || {}
   }
 
-  process(css) {
-    // Check if css is wrapped with any one of the marks
-    const antDesignTemplateMark = this.options.wrappers.reduce((mark, wrapper) => (css.indexOf(wrapper) >= 0 ? wrapper : mark), '')
+  Processor.prototype = {
+    process: function (css) {
+      // Check if css is wrapped with any one of the marks
+      const antDesignTemplateMark = this.options.wrappers.reduce((mark, wrapper) => (css.indexOf(wrapper) >= 0 ? wrapper : mark), '')
 
-    if (!antDesignTemplateMark) {
-      return css
-    }
-
-    // Escape the mark to be RegExp friendly
-    const escapedMark = antDesignTemplateMark.replace(/[.*+?^${}()|[\]\\]/g, '\\$&')
-    // Split, map, remove all, add one, clean double spaces
-    const lines = css.split('\n').map((line) => {
-      if (line.indexOf(antDesignTemplateMark) === -1) {
-        return line
+      if (!antDesignTemplateMark) {
+        return css
       }
 
-      const finder = new RegExp(escapedMark, 'g')
-      const finderNested = new RegExp(`(?<!^(?<!s))${escapedMark} `, 'g')
-      const replaceNestedLine = line.replace(finderNested, '')
-      const replaceLine = `${antDesignTemplateMark} ${replaceNestedLine.replace(finder, '')}`
-      const removeTwoSpace = replaceLine.replace(/( {2})/g, ' ')
-      const final = removeTwoSpace
+      // Escape the mark to be RegExp friendly
+      const escapedMark = antDesignTemplateMark.replace(/[.*+?^${}()|[\]\\]/g, '\\$&')
+      // Split, map, remove all, add one, clean double spaces
+      const lines = css.split('\n').map((line) => {
+        if (line.indexOf(antDesignTemplateMark) === -1) {
+          return line
+        }
 
-      return final
-    })
+        const finder = new RegExp(escapedMark, 'g')
+        const finderNested = new RegExp(`(?<!^(?<!s))${escapedMark} `, 'g')
+        const replaceNestedLine = line.replace(finderNested, '')
+        const replaceLine = `${antDesignTemplateMark} ${replaceNestedLine.replace(finder, '')}`
+        const removeTwoSpace = replaceLine.replace(/( {2})/g, ' ')
+        const final = removeTwoSpace
 
-    return lines.join('\n')
-  }
-}
+        return final
+      })
 
-module.exports = class LessPlugin {
-  constructor(options) {
-    this.options = options
-    this.install = this.install.bind(this)
-    this.PostProcessor = null
-    this.PreProcessor = null
-    this.minVersion = [0, 0, 1]
-
-    this.setOptions = (theOptions) => {
-      this.options = JSON.parse(theOptions.replace(/(\r\n|\n|\r)/gm, '')) // parse options
+      return lines.join('\n')
     }
   }
 
-  install(less, pluginManager) {
-    // this.PreProcessor = preProcessor(less.FileManager)
-    // pluginManager.addPreProcessor(new this.PreProcessor({
-    //   ...this.options,
-    //   less,
-    // }))
-
-    this.PostProcessor = postProcessor(less.FileManager)
-    pluginManager.addPostProcessor(new this.PostProcessor({
-      ...this.options,
-      less,
-    }))
-  }
+  return Processor;
 }
+
+registerPlugin({
+  install: function (less, pluginManager) {
+    var PostProcessor = GetPostProcessor(less)
+    pluginManager.addPostProcessor(new PostProcessor(this.options))
+  },
+  setOptions: function (theOptions) {
+    this.options = JSON.parse(theOptions.replace(/(\r\n|\n|\r)/gm, ''))
+  },
+})
