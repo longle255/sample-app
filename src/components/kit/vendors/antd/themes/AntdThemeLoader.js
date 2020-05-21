2cf50d5d1be01a14f72fa4d9e9e2c0c39401c21c
Sellpixels Robot <nick@mediatec.org>
2020-05-21 16:05:23 +0300

    AntdThemeLoader refactoring (@boskiv)

--- src/components/kit/vendors/antd/themes/AntdThemeLoader.js
+++ src/components/kit/vendors/antd/themes/AntdThemeLoader.js
@@ -1,16 +1,13 @@
 /* eslint-disable */
-var GetPostProcessor = function() {
-  function Processor(options) {
-    this.options = options || {}
+const GetPostProcessor = function () {
+  function Processor(themeName) {
+    this.options = { wrappers: [`[data-kit-theme="${themeName}"]`] } || {}
   }
 
   Processor.prototype = {
-    process: function(css) {
+    process: function (css) {
       // Check if css is wrapped with any one of the marks
-      const antDesignTemplateMark = this.options.wrappers.reduce(
-        (mark, wrapper) => (css.indexOf(wrapper) >= 0 ? wrapper : mark),
-        '',
-      )
+      const antDesignTemplateMark = this.options.wrappers.reduce((mark, wrapper) => (css.indexOf(wrapper) >= 0 ? wrapper : mark), '')
 
       if (!antDesignTemplateMark) {
         return css
@@ -19,7 +16,7 @@ var GetPostProcessor = function() {
       // Escape the mark to be RegExp friendly
       const escapedMark = antDesignTemplateMark.replace(/[.*+?^${}()|[\]\\]/g, '\\$&')
       // Split, map, remove all, add one, clean double spaces
-      const lines = css.split('\n').map(line => {
+      const lines = css.split('\n').map((line) => {
         if (line.indexOf(antDesignTemplateMark) === -1) {
           return line
         }
@@ -28,25 +25,22 @@ var GetPostProcessor = function() {
         const finderNested = new RegExp(`(?<!^(?<!s))${escapedMark} `, 'g')
         const replaceNestedLine = line.replace(finderNested, '')
         const replaceLine = `${antDesignTemplateMark} ${replaceNestedLine.replace(finder, '')}`
-        const removeTwoSpace = replaceLine.replace(/( {2})/g, ' ')
-        const final = removeTwoSpace
-
-        return final
+        return replaceLine.replace(/( {2})/g, ' ')
       })
 
       return lines.join('\n')
-    },
+    }
   }
 
-  return Processor
+  return Processor;
 }
 
-registerPlugin({
-  install: function(less, pluginManager) {
-    var PostProcessor = GetPostProcessor(less)
-    pluginManager.addPostProcessor(new PostProcessor(this.options))
+module.exports = {
+  install: function (less, pluginManager, functions) {
+    const PostProcessor = GetPostProcessor(less)
+    functions.add('apply', function (theme) {
+      pluginManager.addPostProcessor(new PostProcessor(theme.value))
+      return false;
+    });
   },
-  setOptions: function(theOptions) {
-    this.options = JSON.parse(theOptions.replace(/(\r\n|\n|\r)/gm, ''))
-  },
-})
+}
