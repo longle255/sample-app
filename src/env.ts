22c974f93be47d06da2f812276f47077189860b0
Long Le <longle@MacBook-Pro.local>
2020-06-24 12:08:52 +0200

    fix agenda warning

--- src/env.ts
+++ src/env.ts
@@ -76,8 +76,13 @@ export const env = {
     db: {
       address: getOsEnv('MONGO_URI'),
       collection: getOsEnv('AGENDA_COLLECTION_NAME'),
+      // hack for agenda DB deprecated options warning
       options: {
         useNewUrlParser: true,
+        useUnifiedTopology: true,
+        autoReconnect: undefined,
+        reconnectTries: undefined,
+        reconnectInterval: undefined,
       },
     },
   },
