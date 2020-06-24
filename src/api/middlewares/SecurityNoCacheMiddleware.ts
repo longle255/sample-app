f559732c197a666b7b463e1daf8ce95650f37d27
Long Le <longle@MacBook-Pro.local>
2020-06-24 11:23:52 +0200

    fixing deprecated warning

--- src/api/middlewares/SecurityNoCacheMiddleware.ts
+++ src/api/middlewares/SecurityNoCacheMiddleware.ts
@@ -1,10 +1,10 @@
 import { Context } from 'koa';
-import helmet from 'koa-helmet';
+import nocache from 'nocache';
 import { KoaMiddlewareInterface, Middleware } from 'routing-controllers';
 
 @Middleware({ type: 'before' })
 export class SecurityNoCacheMiddleware implements KoaMiddlewareInterface {
   public use(ctx: Context, next: (err?: any) => Promise<any>): Promise<any> {
-    return helmet.noCache()(ctx, next);
+    return (nocache as any)()(ctx.req, ctx.res, next);
   }
 }
