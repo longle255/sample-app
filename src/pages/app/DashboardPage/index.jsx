cc91a2146c4cb5a6ec96cb66ef4e8c2b1503f6cd
Long Le <longle@MacBook-Pro.local>
2020-07-13 19:53:29 +0200

    adding overlay component

--- src/pages/app/DashboardPage/index.jsx
+++ src/pages/app/DashboardPage/index.jsx
@@ -1,13 +1,15 @@
 import React from 'react';
 import { Helmet } from 'react-helmet';
 import ACL from 'components/app/ACL';
+import Overlay from 'components/app/Overlay';
 import Dashboard from './Dashboard';
 
 const DashboardPage = () => {
   return (
     <ACL roles={['admin', 'user']} redirect>
-      <Helmet title="Sign In" />
+      <Helmet title="Dashboard" />
       <Dashboard />
+      <Overlay />
     </ACL>
   );
 };
