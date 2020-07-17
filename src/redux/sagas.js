4752bb337cd0f4021cc420f868a8315d58818f37
Long Le <longle@MacBook-Pro.local>
2020-07-17 11:06:10 +0200

    remove socket saga

--- src/redux/sagas.js
+++ src/redux/sagas.js
@@ -3,8 +3,7 @@ import user from './user/sagas';
 import settings from './settings/sagas';
 import auth from './auth/sagas';
 import referrals from './referrals/sagas';
-import websocket from './websocket/sagas';
 
 export function* rootSaga() {
-  yield all([auth(), user(), settings(), referrals(), websocket()]);
+  yield all([auth(), user(), settings(), referrals()]);
 }
